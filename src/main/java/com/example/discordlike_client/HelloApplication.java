f3d694cd9d3bccafbea83d2692edca167e01b432
QuentinPOL <quentinpol140@gmail.com>
2025-01-23 20:28:45 +0100

    First commmit

---
+++ src/main/java/com/example/discordlike_client/HelloApplication.java
@@ -0,0 +1,30 @@
+package com.example.discordlike_client;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Charger le fichier FXML
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+
+        // Créer une scène avec le fichier FXML
+        Scene scene = new Scene(root);
+
+        // Configurer la fenêtre
+        primaryStage.setTitle("Connexion");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false); // Rendre la fenêtre non redimensionnable
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
